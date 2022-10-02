ed511bfa00e513a1eb8d22b7bf16706d0ee9e505
Nallekari <tuomas.nallikari@outlook.com>
2022-10-02 20:31:35 +0300

    Menköön

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoTable from './TodoTable'
 
 function TodoList(props) {
 
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value });
+  }
+
+  
+  function deleteTodo(index) {
+    props.todos.filter((todo, i) => i !== index)
+  }
+
   return (
     <div>
-    <table>
-                    <tr>
-                        <th>Date</th>
-                        <th>Description</th>
-                    </tr>
-      <tbody>
-        {
-          props.todos.map((todo, index) =>
-              <tr key={index}>
-                <td>{todo.date}</td>
-                <td>{todo.desc}</td>
-              </tr>
-          )
-        }
-      </tbody>
-    </table>
+      <p>Add todo:</p>
+      <form onSubmit={addTodo}>  
+        Description:<input type="text" name="desc" value={todo.desc} onChange={inputChanged} />
+        Date:<input type="text" name="date" value={todo.date} onChange={inputChanged}/>
+        <input type="submit" value="Add"/>
+      </form>
+      <TodoTable todos={todos} />
     </div>
   );
 };
---
+++ src/TodoTable.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function TodoTable(props) {
+    return (
+    <div>
+        <table>
+            <tr>
+                <th>Date</th>
+                <th>Description</th>
+            </tr>
+            <tbody>
+                {
+                props.todos.map((todo, index) =>
+                    <tr key={index}>
+                        <td>{todo.date}</td>
+                        <td>{todo.desc}</td>
+                        <button onClick={props.todos.filter((todo, i) => i !== index)}>Delete</button>
+                    </tr>
+                )
+                }
+            </tbody>
+        </table>
+        </div >
+    );
+} 
+export default TodoTable;
